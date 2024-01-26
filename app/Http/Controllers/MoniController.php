b18a6431728f36a568b25a16c5549c4cd644c10e
davidemonfasani <davide.monfasani.studenti@isii.it>
2024-01-26 12:46:30 +0100

    Yea

--- app/Http/Controllers/MoniController.php
+++ app/Http/Controllers/MoniController.php
@@ -1,11 +1,53 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Monitoraggio;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Validation\ValidationException;
 class MoniController extends Controller
+
 {
-    //
+    
+    
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'id_Sensor'=> [
+                    'required',
+                    'integer',
+                    'exists:Sensors,id_Sensor',
+                ],
+                'Temperatura'=> [
+                    'required',
+                    'integer',
+                ],
+                'Umidità'=> [
+                    'required',
+                    'integer',
+                ],
+                'Peso'=> [
+                    'required',
+                    'integer',
+                ],
+            ]);
+        }
+        catch (ValidationException $e) {
+            $userEmail = Sensor::find($request->id_Sensor)->cellar->user->email;
+            Mail::to($userEmail)->send(new ValidationError($e->errors()));
+    
+            throw $e;
+        }
+        catch (\Exception $e) {
+            Log::error('Exception');
+            Log::error($e->getMessage());
+            return response()->json(['error' => $e->getMessage()], 404);
+        }
+    }
+    
+    
+
+
 }
